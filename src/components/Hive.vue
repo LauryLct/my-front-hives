58c6083f6dd7ccccd20dd59d55a2a148ee1ba872
Lecaut Laury <laury.lecaut@gmail.com>
2020-09-19 19:11:47 +0200

    add axios in Home.vue and Hive.vue

---
+++ src/components/Hive.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class='container'>
+    <router-link to='/'>retour</router-link>
+
+    <div class='hiveInfo'>
+      <div class='hive-name h4'>
+        <h3>Le nom de la ruche:</h3>
+
+        {{hive.name}}
+      </div>
+      <div class='hive-weight h4'>
+        <h3>Le poids de la ruche:</h3>
+        {{hive.weight}}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hive',
+  data() {
+    return { hive: []}
+  },
+
+  created () {
+    const hiveId = this.$route.params.id
+    this.$http.get(`http://localhost:3000/api/v1/hives/${hiveId}`)
+      .then(response => {
+        this.hive = response.data
+    })
+  },
+}
+</script>
+
+// <!-- Add 'scoped' attribute to limit CSS to this component only -->
+<style scoped>
+
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+
+  margin: 0 10px;
+  padding: 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,38 +1,73 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class='hive container text-center'>
+    <h1 class='p-3'>Liste de vos ruches</h1>
+
+    <div class='hiveList'>
+      <ul>
+        <li v-for='hive in hives' :key='hive.id' :hive='hive'>
+          {{hive.name}}
+          <router-link :to='`/hives/${hive.id}`'>voir</router-link>
+        </li>
+      </ul>
+    </div>
+
+    <div class='hive-form'>
+      <h3 class='pb-4'>Ajoutez votre propre ruche</h3>
+      <form @submit='addHive'>
+        <div class='form-group'>
+          <input type='text'
+                class='form-control'
+                v-model='newHive.name'
+                placeholder='Ajoutez le nom de votre ruche'>
+        </div>
+        <div class='form-group'>
+          <input type='text'
+                class='form-control'
+                id='exampleInputPassword1'
+                v-model='newHive.weight'
+                placeholder='Ajoutez le poids de votre ruche'>
+        </div>
+        <button type='submit' class='btn btn-primary'>Ajoute ta ruche</button>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
+
   name: 'Home',
+
+  data() {
+    return{
+      hives: [],
+      newHive: []
+    }
+  },
+  created () {
+    this.$http.get('http://localhost:3000').then(response => {
+      this.hives = response.data
+    })
+  },
+
+  methods: {
+    addHive() {
+      const value = this.newHive
+      if(!value) {
+        return
+      }
+      this.$http.post('http://localhost:3000/api/v1/hives', {
+        hive: {
+          name: this.newHive.name,
+          weight: this.newHive.weight}})
+          .then(response => {
+            this.hives.push(response.data)
+            this.newHive= ''
+          })
+          .catch(error => this.setError(error,"impossible d'ajouter la ruche"))
+    }
+  },
+
   computed: {
     hiveList() {
       return this.getters.home
@@ -41,7 +76,7 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+// <!-- Add 'scoped' attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
   margin: 40px 0 0;
@@ -51,10 +86,16 @@ ul {
   padding: 0;
 }
 li {
-  display: inline-block;
+
   margin: 0 10px;
+  padding: 10px;
 }
 a {
   color: #42b983;
+}
+
+.form-group {
+  padding-left: 10%;
+  padding-right: 10%;
 }
 </style>
